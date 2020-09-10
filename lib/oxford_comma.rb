5bde6718f364ebdf8adc42dae7614bb40756e01a
Alie Stone <helloaliestone@gmail.com>
2020-09-10 17:19:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(arr)
         arr.join
     elsif arr.length == 2
         arr.join(" and ")
-    else 
-      [arr[0..-2].join(', '), arr.last].join(', and ')
+    else
+      [arr[0..-2].join(', '), [arr.last].join(', and ')
     end
 end
